3c91650cc04a63ef8c960583ab418ff75bb3a652
yufuchang <yufuchang@inspur.com>
2021-05-31 10:26:29 +0800

    项目2提交

---
+++ src/main/java/com/yufuchang/Springboot002Application.java
@@ -0,0 +1,13 @@
+package com.yufuchang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot002Application.class, args);
+    }
+
+}
